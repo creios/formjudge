b906bcbf4f5b4d2939736f6495caccbf88627262
Tim Tegeler <tim@tegeler.me>
2017-02-21 13:58:55 +0100

    Added FieldGenerator

---
+++ phpunit/tests/Generator/FieldGeneratorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Creios\FormJudge\Generator;
+
+use Creios\FormJudge\Fields\Field;
+use PHPUnit_Framework_MockObject_MockObject;
+
+class FieldGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+
+    /** @var PHPUnit_Framework_MockObject_MockObject|Field */
+    private $field;
+
+    public function setUp()
+    {
+        $this->field = $this->getMock(Field::class);
+    }
+
+    public function testGenerate()
+    {
+        $this->field->method('getType')->willReturn('email');
+        $this->field->method('getLengthMaxConstraint')->willReturn(null);
+        $this->field->method('getLengthMinConstraint')->willReturn(null);
+        $this->field->method('getMaxConstraint')->willReturn(null);
+        $this->field->method('getMinConstraint')->willReturn(null);
+        $this->field->method('getMandatoryConstraint')->willReturn(null);
+        $this->field->method('getPatternConstraint')->willReturn(null);
+        $fieldGenerator = new FieldGenerator($this->field);
+        $expectedFieldAttributes = 'type="email" ';
+        $this->assertEquals($expectedFieldAttributes, $fieldGenerator->generate());
+    }
+}
